08f489d1ab03d72bcd83bf47559039e0f527824d
jma26 <j.masonoma17@gmail.com>
2018-05-15 22:59:21 -0700

    Add services folder with initial login, registration service setup

---
+++ hungryhippoApp/src/app/services/register.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class RegisterService {
+
+  constructor(private _http: HttpClient) { }
+
+  registerUser(newUser) {
+    console.log('Register.service.ts pinging ' + newUser);
+    return this._http.post('/registration', newUser);
+  }
+  
+}
