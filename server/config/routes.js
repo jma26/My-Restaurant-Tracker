26d890f178c408f81b6e4928c1b3ca8d32ae072d
jma26 <j.masonoma17@gmail.com>
2018-05-15 23:12:50 -0700

    Initial route setup for user login, registration

---
+++ server/config/routes.js
@@ -0,0 +1,21 @@
+const Users = require('../controllers/users.js');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+module.exports = function(app) {
+    // Create a new user
+    app.post('/registration', function(request, response) {
+        console.log('Creating new user, @routes.js pinging');
+        Users.create(request, response);
+    })
+    // Send login information and validate
+    app.post('/login', function(request, response) {
+        console.log('Retrieving user for validation, @routes.js pinging');
+        Users.login(request, response);
+    })
+    // Retrieve user profile
+    app.get('/user/:alias', function(request, response) {
+        console.log('Retrieving user profile, @routes.js pinging');
+        Users.getUser(request, response);
+    })
+}
