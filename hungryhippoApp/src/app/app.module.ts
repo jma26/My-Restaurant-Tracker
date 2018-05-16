c8674f2c3ea738aa65153dd11d67bac2c72b658e
jma26 <j.masonoma17@gmail.com>
2018-05-15 23:59:14 -0700

    Add RouterModule and appRoutes

--- hungryhippoApp/src/app/app.module.ts
+++ hungryhippoApp/src/app/app.module.ts
@@ -1,23 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+// Services
+import { LoginService } from './services/login.service';
+import { RegisterService } from './services/register.service';
 
 import { AgmCoreModule } from '@agm/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginRegistrationComponent } from './login-registration/login-registration.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  { path: '', component: LoginRegistrationComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRegistrationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCFDzK21ltulX-A1BxEJgUsrYdrYiPf5gw'
     })
   ],
-  providers: [],
+  providers: [LoginService, RegisterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
