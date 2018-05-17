42008023ac3efb131610cb9dbc072d3b775f583a
jma26 <j.masonoma17@gmail.com>
2018-05-17 14:17:57 -0700

    Update RestaurantSchema with user

--- server/models/restaurant.js
+++ server/models/restaurant.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 // Restaurant schema
 const RestaurantSchema = mongoose.Schema({
-    name: { type: String, required: [true, 'Restaurant name is required']},
+    user: { type: String, required: [true, 'Reviwer\'s name is required']},
+    restaurant: { type: String, required: [true, 'Restaurant name is required']},
     lng: { type: Number, required: [true, 'Longitude is required']},
     lat: { type: Number, required: [true, 'Latitude is required']},
     review: [{
