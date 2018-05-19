c70fb89eb1013f4bba4f17877832fe1a5d7ba78b
Jesse Ma <j.masonoma17@gmail.com>
2018-05-18 22:03:55 -0700

    Add markers array and display on agm-marker

--- hungryhippoApp/src/app/home/home.component.ts
+++ hungryhippoApp/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { google } from '@agm/core/services/google-maps-types';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  markers: any[];
   title = 'RestaurantFive0';
   lat: number = 37.3382082;
   lng: number = -121.8863286;
@@ -16,4 +16,32 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  markers: marker[] = [
+    {
+      lat: 37.431896,
+      lng: -121.903623,
+      label: 'Teaspoon',
+      draggable: false
+    },
+    {
+      lat: 37.402715,
+      lng: -121.933038,
+      label: 'Curry Up Now',
+      draggable: false
+    },
+    {
+    lat: 37.392646,
+    lng: -121.977432,
+    label: 'Get Fried Fry Cafe',
+    draggable: false
+    }
+  ]
+
+}
+
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable: boolean;
 }
