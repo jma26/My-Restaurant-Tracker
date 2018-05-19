db6f3030549fafb3feccb9d636b6db8be311ad17
Jesse Ma <j.masonoma17@gmail.com>
2018-05-18 23:10:24 -0700

    Update model, RestaurantSchema

--- server/models/restaurant.js
+++ server/models/restaurant.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 
 // Restaurant schema
 const RestaurantSchema = mongoose.Schema({
-    user: { type: String, required: [true, 'Reviwer\'s name is required']},
-    restaurant: { type: String, required: [true, 'Restaurant name is required']},
+    restaurant_name: { type: String, required: [true, 'Restaurant name is required']},
     lng: { type: Number, required: [true, 'Longitude is required']},
     lat: { type: Number, required: [true, 'Latitude is required']},
-    review: [{
-        content: { type: String },
-        stars: { type: Number }
-    }],
-    images: [{ data: Buffer, contentType: String }]
+    review_content: [{
+        reviewer: { type: String, required: [true, 'Reviewer\'s name is required']},
+        review: { type: String, required: [true, 'Review can not be blank'] },
+        stars: { type: Number, required: [true, 'Star rating required'], min: 1, max: 5 },
+        images: [{ data: Buffer, contentType: String }]
+    }]
 });
 
 let Restaurant = mongoose.model('Restaurant', RestaurantSchema);
