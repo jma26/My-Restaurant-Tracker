6a7297ba807b28ada8ead8e9cb44a1dcadf64a4b
Jesse Ma <j.masonoma17@gmail.com>
2018-05-29 23:54:16 -0700

    Initial setup for newReview, update longitude, latitude to lng, lat

--- hungryhippoApp/src/app/new-restaurant-review/new-restaurant-review.component.ts
+++ hungryhippoApp/src/app/new-restaurant-review/new-restaurant-review.component.ts
@@ -12,8 +12,8 @@ export class NewRestaurantReviewComponent implements OnInit {
 
   new_review: FormGroup;
   restaurant_name: FormControl;
-  longitude: FormControl;
-  latitude: FormControl;
+  lng: FormControl;
+  lat: FormControl;
   reviewer: FormControl;
   review: FormControl;
   stars: FormControl;
@@ -35,24 +35,34 @@ export class NewRestaurantReviewComponent implements OnInit {
 
   createFormControls() {
     this.restaurant_name = new FormControl("", Validators.required),
-    this.longitude = new FormControl("", Validators.required),
-    this.latitude = new FormControl("", Validators.required),
-    this.reviewer = new FormControl("", Validators.required),
+    this.lng = new FormControl("", Validators.required),
+    this.lat = new FormControl("", Validators.required),
+    this.reviewer = new FormControl(this.fullname, Validators.required),
     this.review = new FormControl("", [Validators.required, Validators.minLength(10)]),
-    this.stars = new FormControl("", Validators.required),
-    this.image = new FormControl(null);
+    this.stars = new FormControl(1, Validators.required),
+    this.image = new FormControl(null)
   }
 
   createForm() {
     this.new_review = new FormGroup({
       restaurant_name: this.restaurant_name,
-      lng: this.longitude,
-      lat: this.latitude,
+      lng: this.lng,
+      lat: this.lat,
       reviewer: this.reviewer,
       review: this.review,
       stars: this.stars,
       image: this.image
     });
+  }
+
+  newReview() {
+    if (this.new_review.valid) {
+      console.log(this.new_review.value);
+      console.log('Form successfully submitted');
+    } else {
+      console.log(this.new_review.value);
+      console.log('Form unsuccessfully submitted');
+    }
   }
 
 
