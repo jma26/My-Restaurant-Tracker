edd73c0816bcc5dd3f5c51df4d4eac3d4e4e80dd
Jesse Ma <j.masonoma17@gmail.com>
2018-05-29 22:26:34 -0700

    Initial setup of form-group, form-control

--- hungryhippoApp/src/app/new-restaurant-review/new-restaurant-review.component.ts
+++ hungryhippoApp/src/app/new-restaurant-review/new-restaurant-review.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-restaurant-review',
@@ -9,13 +10,49 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class NewRestaurantReviewComponent implements OnInit {
   fullname: String;
 
+  new_review: FormGroup;
+  restaurant_name: FormControl;
+  longitude: FormControl;
+  latitude: FormControl;
+  reviewer: FormControl;
+  review: FormControl;
+  stars: FormControl;
+  image: FormControl;
+
+
   constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    // Get user name
     this.route.params.subscribe(params => {
       this.fullname = params['fullname'];
       console.log(this.fullname);
-    })
+    });
+    // Run CreateFormControls, createForm Function
+    this.createFormControls();
+    this.createForm();
+  }
+
+  createFormControls() {
+    this.restaurant_name = new FormControl("", Validators.required),
+    this.longitude = new FormControl("", Validators.required),
+    this.latitude = new FormControl("", Validators.required),
+    this.reviewer = new FormControl("", Validators.required),
+    this.review = new FormControl("", [Validators.required, Validators.minLength(10)]),
+    this.stars = new FormControl("", Validators.required),
+    this.image = new FormControl(null);
+  }
+
+  createForm() {
+    this.new_review = new FormGroup({
+      restaurant_name: this.restaurant_name,
+      lng: this.longitude,
+      lat: this.latitude,
+      reviewer: this.reviewer,
+      review: this.review,
+      stars: this.stars,
+      image: this.image
+    });
   }
 
 
