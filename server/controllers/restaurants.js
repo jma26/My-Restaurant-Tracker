8aa42f622a984681af5b450e26179348c426332d
Jesse Ma <j.masonoma17@gmail.com>
2018-06-08 00:01:08 -0700

    Update add_review for null image & add getReviews

--- server/controllers/restaurants.js
+++ server/controllers/restaurants.js
@@ -5,29 +5,63 @@ const fs = require('fs');
 module.exports = {
     add_review: function(request, response) {
         console.log('Controllers @restaurant.js route pinging', request.body);
-        let buffer = new Buffer(request.body.image.value, 'base64');
-        let restaurant = new Restaurant({
-            restaurant_name: request.body.restaurant_name,
-            lng: request.body.lng,
-            lat: request.body.lat,
-            review_content: [{
-                reviewer: request.body.reviewer,
-                review: request.body.review,
-                stars: request.body.stars,
-                images: [{
-                    data: buffer,
-                    contentType: request.body.image.filetype,
-                    filename: request.body.image.filename
+        if (request.body.image === null) {
+            let restaurant = new Restaurant({
+                restaurant_name: request.body.restaurant_name,
+                lng: request.body.lng,
+                lat: request.body.lat,
+                review_content: [{
+                    reviewer: request.body.reviewer,
+                    review: request.body.review,
+                    stars: request.body.stars
                 }]
-            }]
-        })
-
-        restaurant.save(function(error, result) {
+            })
+            restaurant.save(function(error, result) {
+                if (error) {
+                    console.log('Something went wrong adding a review');
+                    response.json(error);
+                } else {
+                    console.log('Successful new review');
+                    response.json(result);
+                }
+            })
+        } else {
+            let buffer = new Buffer(request.body.image.value, 'base64');
+            let restaurant = new Restaurant({
+                restaurant_name: request.body.restaurant_name,
+                lng: request.body.lng,
+                lat: request.body.lat,
+                review_content: [{
+                    reviewer: request.body.reviewer,
+                    review: request.body.review,
+                    stars: request.body.stars,
+                    images: [{
+                        data: buffer,
+                        contentType: request.body.image.filetype,
+                        filename: request.body.image.filename
+                    }]
+                }]
+            })
+    
+            restaurant.save(function(error, result) {
+                if (error) {
+                    console.log('Something went wrong adding a review');
+                    response.json(error);
+                } else {
+                    console.log('Successful new review');
+                    response.json(result);
+                }
+            })
+        }
+    },
+    getReviews: function(request, response) {
+        console.log('Controllers @restaurant.js route pinging - getReviews');
+        Restaurant.find({}).limit(5).exec(function(error, result) {
             if (error) {
-                console.log('Something went wrong adding a review');
+                console.log('Something went wrong retrieving reviews');
                 response.json(error);
             } else {
-                console.log('Successful new review');
+                console.log('Successful retrieval');
                 response.json(result);
             }
         })
