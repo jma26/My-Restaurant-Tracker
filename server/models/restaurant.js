bf0c0b68e2649f4aa3d25ddf0c9d1956218b1551
Jesse Ma <j.masonoma17@gmail.com>
2018-06-07 23:42:22 -0700

    Add createdAt to RestaurantSchema

--- server/models/restaurant.js
+++ server/models/restaurant.js
@@ -9,6 +9,7 @@ const RestaurantSchema = mongoose.Schema({
         reviewer: { type: String, required: [true, 'Reviewer\'s name is required']},
         review: { type: String, required: [true, 'Review can not be blank'] },
         stars: { type: Number, required: [true, 'Star rating required'], min: 1, max: 5 },
+        created: { type: Date, default: Date.now },
         images: [{ data: Buffer, contentType: String, filename: String }]
     }]
 });
