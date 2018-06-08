9e24c2f75fedbda55803d169284c586defc95410
Jesse Ma <j.masonoma17@gmail.com>
2018-06-07 23:52:24 -0700

    Update routes with .get('/getreviews')

--- server/config/routes.js
+++ server/config/routes.js
@@ -25,4 +25,9 @@ module.exports = function(app) {
         console.log('Creating new restaurant review, @routes.js pinging', request.body);
         Restaurants.add_review(request, response);
     })
+    // Retrieve restaurant reviews
+    app.get('/getreviews', function(request, response) {
+        console.log('Retrieving restaurant reviews, @routes.js pinging');
+        Restaurants.getReviews(request, response);
+    })
 }
