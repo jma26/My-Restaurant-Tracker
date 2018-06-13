2c31b6697664c38354579435a64c5c7494018c06
Jesse Ma <j.masonoma17@gmail.com>
2018-06-13 00:48:36 -0700

    Convert bufferImage to base64 and display image from review if it exists

--- hungryhippoApp/src/app/home/home.component.ts
+++ hungryhippoApp/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ReviewService } from '../services/review.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -17,8 +18,10 @@ export class HomeComponent implements OnInit {
   review: any = false;
   previous;
   stars: String;
+  base64Image: any;
+  bufferImage: any;
 
-  constructor(private route: ActivatedRoute, private router: Router, private _reviewService: ReviewService) {
+  constructor(private route: ActivatedRoute, private router: Router, private _reviewService: ReviewService, private _domSanitizer: DomSanitizer) {
    }
 
   ngOnInit() {
@@ -44,11 +47,13 @@ export class HomeComponent implements OnInit {
     console.log(infowindow);
     this.review = markerInfo;
     console.log(this.review);
+    console.log(this.review.review_content[0].images[0]);
     if (this.previous) {
       this.previous.close();
     }
     this.previous = infowindow;
     this.getStars(this.review);
+    this.getImage(this.review.review_content[0].images[0]);
   }
 
   getStars(review) {
@@ -58,6 +63,18 @@ export class HomeComponent implements OnInit {
       this.stars += "<i class='fas fa-star'></i>";
     }
     console.log(this.stars);
+  }
+
+  getImage(image) {
+    // Check if image exists
+    if (image === undefined || image === null) {
+      return false;
+    }
+    // Save buffer image to base64
+    this.bufferImage = new Buffer(image.data.data).toString('base64');
+    // Sanitize base64 encoded image
+    this.base64Image = this._domSanitizer.bypassSecurityTrustUrl(`data:${image.contentType};base64, ${this.bufferImage}`);
+    console.log(this.base64Image);
   }
 
   logout() {
